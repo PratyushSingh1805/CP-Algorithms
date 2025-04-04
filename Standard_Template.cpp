b6dc99c91d259f7ab9a3f2a2ae4e79d251bc571f
PratyushSingh1805 <168963845+PratyushSingh1805@users.noreply.github.com>
2025-04-04 20:36:18 +0530

    Create Standard_Template.cpp

---
+++ Standard_Template.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i,n) for(ll i = 0 ; i < n ; i++)
+#define rf(i,n) for(ll i = n-1;i>=0;i--)
+#define so(arr) sort(arr.begin(),arr.end())
+#define rs(arr) sort(arr.rbegin(),arr.rend())
+#define rev(arr) reverse(arr.begin(),arr.end())
+#define MOD (ll)1000000007
+#define pb push_back
+#define vl vector<ll>
+#define vc vector<char>
+#define vd vector<double>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvl vector<vector<ll>>
+#define vvc vector<vector<char>>
+#define pl pair<ll,ll>
+#define getarr(arr,n) vector<ll> arr(n);for(ll i=0;i<n;i++){cin>>arr[i];}
+#define printarr(arr,n) for(ll i=0;i<n;i++){cout<<arr[i] <<' ';} cout << endl;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+//order_of_key(x): Returns the count of elements in the set that are strictly less than x.
+//find_by_order(k): Returns an iterator to the k-th element (0-based indexing) in the sorted set.
+// 32-__builtin_clz(num) gives the positon of first 1(assuming first digit as 1)//number of leading zeroes in int
+// 64-__builtin_clzll(num) for ll
+
+
+void solve(){
+    
+}
+ 
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+   ll t=1;
+   cin >> t;
+   while(t--){
+    solve();
+   }
+  return 0;
+}
